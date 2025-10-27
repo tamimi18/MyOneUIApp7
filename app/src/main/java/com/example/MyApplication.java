40809708cda6b4261e9a6d476082d533c8cb5ea6
tamimi18 <mohamad.tamimi18.82@gmail.com>
2025-10-27 21:20:37 +0300

    Update MyApplication.java

--- app/src/main/java/com/example/MyApplication.java
+++ app/src/main/java/com/example/MyApplication.java
@@ -1,13 +1,22 @@
 package com.example.oneuiapp;
 
+import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
+import android.os.Bundle;
 import android.util.Log;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyApplication extends Application {
 
     private static final String TAG = "MyApplication";
     private static MyApplication sInstance;
+
+    // قائمة لتخزين الأنشطة
+    private static final List<WeakReference<Activity>> activities = new ArrayList<>();
 
     @Override
     protected void attachBaseContext(Context base) {
@@ -34,6 +43,23 @@ public class MyApplication extends Application {
         } catch (Exception e) {
             Log.e(TAG, "applyFont failed in onCreate", e);
         }
+
+        // تسجيل الأنشطة
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+                activities.add(new WeakReference<>(activity));
+                // تطبيق الخط مباشرة عند إنشاء أي Activity
+                FontHelper.applyFont(activity);
+            }
+
+            @Override public void onActivityStarted(Activity activity) {}
+            @Override public void onActivityResumed(Activity activity) {}
+            @Override public void onActivityPaused(Activity activity) {}
+            @Override public void onActivityStopped(Activity activity) {}
+            @Override public void onActivitySaveInstanceState(Activity activity, Bundle outState) {}
+            @Override public void onActivityDestroyed(Activity activity) {}
+        });
     }
 
     public static MyApplication getInstance() {
@@ -41,13 +67,14 @@ public class MyApplication extends Application {
     }
 
     /**
-     * Best-effort notify all activities to recreate by broadcasting a font-change action.
+     * إعادة إنشاء كل الأنشطة المفتوحة لتطبيق الخط الجديد
      */
     public void recreateAllActivities() {
-        try {
-            FontChangeBroadcaster.sendFontChangeBroadcast(this);
-        } catch (Exception e) {
-            Log.e(TAG, "recreateAllActivities failed", e);
+        for (WeakReference<Activity> ref : activities) {
+            Activity act = ref.get();
+            if (act != null && !act.isFinishing()) {
+                act.recreate();
+            }
         }
     }
 }
