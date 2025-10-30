62d15270ef95e9df0bed9b605fc0f35f5d861de4
tamimi18 <mohamad.tamimi18.82@gmail.com>
2025-10-30 04:04:15 +0300

    Update MyApplication.java

--- app/src/main/java/com/example/oneuiapp/MyApplication.java
+++ app/src/main/java/com/example/oneuiapp/MyApplication.java
@@ -12,13 +12,10 @@ import java.util.List;
 public class MyApplication extends Application {
 
     private static MyApplication sInstance;
-
-    // قائمة لتخزين الأنشطة
     private static final List<WeakReference<Activity>> activities = new ArrayList<>();
 
     @Override
     protected void attachBaseContext(Context base) {
-        // نغلف الـ Context بالـ Locale + Font مبكرًا
         super.attachBaseContext(SettingsHelper.wrapContext(base));
         sInstance = this;
     }
@@ -27,20 +24,11 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         sInstance = this;
-
-        // تهيئة الـ CrashHandler
         CrashHandler.init(this);
-
-        // تطبيق الثيم من الإعدادات
         SettingsHelper.initializeFromSettings(this);
 
-        // تسجيل الأنشطة
         registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
-            @Override
-            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
-                activities.add(new WeakReference<>(activity));
-            }
-
+            @Override public void onActivityCreated(Activity activity, Bundle savedInstanceState) { activities.add(new WeakReference<>(activity)); }
             @Override public void onActivityStarted(Activity activity) {}
             @Override public void onActivityResumed(Activity activity) {}
             @Override public void onActivityPaused(Activity activity) {}
@@ -50,19 +38,12 @@ public class MyApplication extends Application {
         });
     }
 
-    public static MyApplication getInstance() {
-        return sInstance;
-    }
+    public static MyApplication getInstance() { return sInstance; }
 
-    /**
-     * إعادة إنشاء كل الأنشطة المفتوحة لتطبيق الخط الجديد
-     */
     public void recreateAllActivities() {
         for (WeakReference<Activity> ref : activities) {
             Activity act = ref.get();
-            if (act != null && !act.isFinishing()) {
-                act.recreate();
-            }
+            if (act != null && !act.isFinishing()) act.recreate();
         }
     }
 }
